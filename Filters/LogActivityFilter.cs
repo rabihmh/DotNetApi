1d999c7f892ad521aa060bf00f182bc6fe711159
rabihmh <76842351+rabihmh@users.noreply.github.com>
2024-02-11 20:02:31 +0200

    Filters

---
+++ Filters/LogActivityFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DotNetApi.Filters
+{
+    public class LogActivityFilter:IActionFilter
+    {
+        private readonly ILogger _logger;
+
+        public LogActivityFilter(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using DotNetApi.Data;
+using DotNetApi.Filters;
 using DotNetApi.Middlewares;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,7 +13,11 @@ namespace DotNetApi
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            //builder.Services.AddControllers(options =>options.Filters.Add<LogActivityFilter>());
+            builder.Services.AddControllers(options =>
+            {
+                options.Filters.Add<LogActivityFilter>();
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
